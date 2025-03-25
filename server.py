a2e878eeef05789203333275e4e46c8a42b9fe4a
yusefjawad03 <112405996+yusefjawad03@users.noreply.github.com>
2025-03-25 04:26:24 -0400

    Update server.py

--- server.py
+++ server.py
@@ -109,10 +109,6 @@ def calculate():
         scholar_name = data['scholarName']
         linkedin_username = data['linkedinUsername']
         website_url = data['websiteUrl']
-        headers = {"X-RapidAPI-Key": "7c963ab537msh1c91075acd364b3p1ff392jsnf523b930f1dd", "X-RapidAPI-Host": "linkedin-data-api.p.rapidapi.com"}
-        scholar_api_key = "0b336f30e9feb2ef6991a61edda58e813bbf368bd6e8a9c92f2f0c0a8bff8482"
-        safe_browsing_api_key = "AIzaSyCAPWZUkjobj7T-mX2p0kI5A3PeGSrn5as"
-        domain_api_key = "7c963ab537msh1c91075acd364b3p1ff392jsnf523b930f1dd"
         profile_data = get_linkedin_profile(linkedin_username, headers)
         scholar_data = get_google_scholar_data(scholar_name, scholar_api_key)
         if profile_data or scholar_data:
